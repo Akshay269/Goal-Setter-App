6038d921df1d15d0f69881154624032608a3b121
Akshay <aksrahangdale@gmail.com>
2022-08-12 22:58:17 +0530

    debug12aug

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,8 +6,8 @@ const {
   getMe,
 } = require('../controllers/userController')
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
-router.get('/me', getMe)
+router.post('/',registerUser)
+router.post('/login',loginUser)
+router.get('/me',getMe)
 
-module.exports = router
+module.exports = router;
