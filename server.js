5580c088c80aeabb0ad537b0ab287dd72c46f9cb
Akshay <aksrahangdale@gmail.com>
2023-03-02 23:30:31 +0530

    ji

--- server.js
+++ server.js
@@ -7,31 +7,26 @@ const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 
-
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+connectDB();
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, 'frontend','build', 'index.html')
     )
   );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
 
-app.use(errorHandler);
 
-connectDB();
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
